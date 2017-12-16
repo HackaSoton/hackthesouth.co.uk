0b60ec3a5a708844e9e43d4971324a7d7c86b0be
Alexandru Rosianu <me@aluxian.com>
2017-12-16 21:23:39 +0200

    Simplify

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,10 @@
-import tachyons from 'tachyons/css/tachyons.min.css'
-import hack from '../static/fonts/hack-subset.css'
-
-import Document, { Head, Main, NextScript } from 'next/document'
-
-const GLOBAL_CSS = tachyons + hack + `
-  html {
-    font-family: Hack, monospace;
-  }
-`;
+import Document, {Head, Main, NextScript} from 'next/document'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <html>
         <Head>
-          <title>Hack The South UK</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
@@ -24,7 +14,6 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
           <meta name="msapplication-config" content="/static/favicons/browserconfig.xml" />
           <meta name="theme-color" content="#feda44" />
-          <style jsx global>{GLOBAL_CSS}</style>
         </Head>
         <body className="bg-black white">
           <Main />
