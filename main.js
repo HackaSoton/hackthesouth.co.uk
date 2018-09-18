ee7cb399cd1f47e17014941c371e53c59eda5acd
Alexandru Rosianu <me@aluxian.com>
2018-09-18 17:43:15 +0200

    add amex & GA events

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-var scroll = new SmoothScroll('nav a[href*="#"]', {
+// smooth scroll
+
+new SmoothScroll('nav a[href*="#"]', {
   speed: 500,
   offset: 50,
   easing: 'easeInOutCubic',
+});
+
+// google analytics event reporting
+
+document.querySelectorAll('[ga-event-action]').forEach(function(el) {
+  el.addEventListener('click', function() {
+    var eventAction = el.getAttribute('ga-event-action');
+    var eventCategory = el.getAttribute('ga-event-category');
+    var eventLabel = el.getAttribute('ga-event-label');
+    var eventValue = el.getAttribute('ga-event-value');
+
+    if (!eventValue && el.nodeName === 'A') {
+      eventValue = el.getAttribute('href');
+    }
+
+    gtag('event', eventAction, {
+      event_category: eventCategory,
+      event_label: eventLabel,
+      value: eventValue
+    });
+  });
 });
